7f775675859a23595d0fad40da03962af3420a18
ruiyang00 <reyyang00@gmail.com>
2019-11-07 17:55:36 -0800

    add two private APIs to routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -12,13 +12,13 @@ var facebookToken = passport.authenticate('facebookToken', { session: false });
 
 //restful APIs
 router.route('/addUserPro')
-  .post(UserController.updateUserPro);
+    .post(UserController.updateUserPro);
 
 router.route('/signup')
     .post(validateBody(schemas.authSchema), UserController.signUp);
 
 
-
+// these are public APIs
 router.route('/signin')
     .post(validateBody(schemas.authSchema), passportSignIn, UserController.signIn);
 
@@ -28,7 +28,19 @@ router.route('/oauth/google')
 router.route('/oauth/facebook')
     .post(facebookToken, UserController.facebookOAuth);
 
+
+
+//demo
 router.route('/secret')
+    .get(passportJWT, UserController.secret);
+
+
+
+// below two APIs can be called after user being authenticated
+router.route('/getrooms') 
+    .get(passportJWT, UserController.secret);
+
+router.route('/getroommates')
     .get(passportJWT, UserController.secret);
 
 module.exports = router;
